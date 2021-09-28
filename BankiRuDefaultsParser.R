3d7f234db93776a5394c57f2a94d06c74973b66a
IvanDublsSt <73642534+IvanDublsSt@users.noreply.github.com>
2021-09-28 19:24:22 +0300

    .

--- BankiRuDefaultsParser.R
+++ BankiRuDefaultsParser.R
@@ -5,17 +5,24 @@ library(stringr)
 setwd("C:/Users/Fride/OneDrive/Документы/GitHub/Coursework")
 
 GetNumberOfPages
-
-
-OnePageProcessing <- function(Page){
 OnePageHTML <- read_html("https://www.banki.ru/banks/memory/?PAGEN_1=1")
-OnePageItems <- xml_find_all(OnePageHTML, ".//tr[@data-test = 'memory-book-item']")
-OnePageOneItem <- OneItemProcessing(OnePageItems[[1]])
-OnePageItemsText <- lapply(OnePageItems, OneItemProcessing)
-OnePageItemsText <- do.call(rbind, OnePageItemsText)
-OnePageItemsText <- as.data.table(OnePageItemsText)
-colnames(OnePageItemsText) <- c("BankDefaultIndex", "Name", "regnum", "DefaultType", "DefaultDate", "BankLocalization")
-return(OnePageItemsText)
+OnePageHeader <- xml_find_all(OnePageHTML, ".//div[@class = 'margin-bottom-default']")[[1]]
+OnePageNumbersAttribute <- xml_attr(OnePageHeader, "data-options")
+OnePageNumbersAttributeSplit <- str_split(OnePageNumbersAttribute, "\n")[[1]]
+OnePageItemsPerPage <- OnePageNumbersAttributeSplit[str_detect(OnePageNumbersAttributeSplit, "itemsPerPage")]
+OnePageItemsPerPage <- as.numeric(str_extract(OnePageItemsPerPage, "\\d+"))
+OnePageTotalItems <- OnePageNumbersAttributeSplit[str_detect(OnePageNumbersAttributeSplit, "itemsPerPage")]
+OnePageTotalItems <- as.numeric(str_extract(OnePageTotalItems, "\\d+"))
+
+OnePageProcessing <- function(URL){
+  OnePageHTML <- read_html(URL)
+  OnePageItems <- xml_find_all(OnePageHTML, ".//tr[@data-test = 'memory-book-item']")
+  OnePageOneItem <- OneItemProcessing(OnePageItems[[1]])
+  OnePageItemsText <- lapply(OnePageItems, OneItemProcessing)
+  OnePageItemsText <- do.call(rbind, OnePageItemsText)
+  OnePageItemsText <- as.data.table(OnePageItemsText)
+  colnames(OnePageItemsText) <- c("BankDefaultIndex", "Name", "regnum", "DefaultType", "DefaultDate", "BankLocalization")
+  return(OnePageItemsText)
 }
 
 OneItemProcessing <- function(Item){
